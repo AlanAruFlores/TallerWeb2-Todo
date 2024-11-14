581591cd470e439bfc2ac796a3fa293f1e0c7e0e
AlanAruFlores <alanaruquipa242@gmail.com>
2024-11-13 21:44:03 -0300

    nuevos modulos y obteniendo tareas de la api

--- TW2Todofrontend/src/app/app.module.ts
+++ TW2Todofrontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import { ItemComponent } from './components/item/item.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TW2Todofrontend/src/app/components/main/main.component.ts
+++ TW2Todofrontend/src/app/components/main/main.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tarea } from 'src/app/models/tarea';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  httpClient : HttpClient;
+
+  constructor(httpClient:HttpClient) {
+    this.httpClient = httpClient;
+   }
 
   ngOnInit(): void {
+    let tareasObservable: Observable<Tarea[]>  = this.httpClient.get<Tarea[]>("http://localhost:3000/api/tarea");
+    tareasObservable.subscribe(valor =>{
+      console.log(valor);
+    })
   }
 
 }
